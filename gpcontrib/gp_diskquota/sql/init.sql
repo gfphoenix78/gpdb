1b1a503387af1032cb6979180e52892e27d7bc56
Hao Wu <gfphoenix78@gmail.com>
2018-11-15 07:49:17 +0000

    Update test cases

--- gpcontrib/gp_diskquota/sql/init.sql
+++ gpcontrib/gp_diskquota/sql/init.sql
@@ -1,17 +1,21 @@
+-- start_ignore
 \! gpconfig -c shared_preload_libraries -v diskquota > /dev/null
 -- end_ignore
-
 \! echo $?
 
+-- start_ignore
 \! gpconfig -c diskquota.monitor_databases -v contrib_regression > /dev/null
 -- end_ignore
+\! echo $?
 
+-- start_ignore
+\! gpconfig -c diskquota.naptime -v 2 > /dev/null
+-- end_ignore
 \! echo $?
 
 -- start_ignore
 \! gpstop -rai > /dev/null
 -- end_ignore
-
 \! echo $?
 
 \! sleep 10
